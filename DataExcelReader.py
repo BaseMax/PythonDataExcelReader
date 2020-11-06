ff43c7510740d98217497c935c02f69e3a83fe25
Max Base <MaxBaseCode@Gmail.Com>
2020-11-06 17:26:43 +0330

    Create DataExcelReader.py

---
+++ DataExcelReader.py
@@ -0,0 +1,22 @@
+# Max Base
+# 
+import json
+
+def readExel(fileTxt):
+    reader = open(fileTxt)
+    try:
+        context=reader.read()
+        context=context.strip()
+        # print(context)
+        rows=context.split('\n')
+        # print(rows)
+        for i in range(len(rows)):
+            rows[i]=rows[i].split('\t')
+            # print(rows[i])
+        return rows
+    finally:
+        reader.close()
+    return []
+
+arr=readExel('input.txt')
+print(arr)
